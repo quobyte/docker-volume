862ce88aaf99f22dd54364387dd355f1932be326
Silvan Kaiser <silvan@quobyte.com>
2017-12-15 15:23:55 +0100

    Read params via environment instead of cli flags
    Instead of reading the parameters to the docker-quobyte-plugin service
    via cli flags they are now read directly from the environment. This
    prevents credential leakage for the API password.
    A tenant_id is now provided via the configuration (not via cli param).

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/docker/go-plugins-helpers/volume"
 )
 
-const quobyteID string = "quobyte"
+const (
+	quobyteID string = "quobyte"
+)
 
 var (
 	version  string
@@ -16,42 +19,52 @@ var (
 )
 
 func main() {
-	quobyteMountPath := flag.String("path", "/run/docker/quobyte/mnt", "Path where Quobyte is mounted on the host")
-	quobyteMountOptions := flag.String("options", "-o user_xattr", "Fuse options to be used when Quobyte is mounted")
-
-	quobyteUser := flag.String("user", "root", "User to connect to the Quobyte API server")
-	quobytePassword := flag.String("password", "quobyte", "Password for the user to connect to the Quobyte API server")
-	quobyteConfigName := flag.String("configuration_name", "BASE", "Name of the volume configuration of new volumes")
-	quobyteAPIURL := flag.String("api", "http://localhost:7860", "URL to the API server(s) in the form http(s)://host[:port][,host:port] or SRV record name")
-	quobyteRegistry := flag.String("registry", "localhost:7861", "URL to the registry server(s) in the form of host[:port][,host:port] or SRV record name")
-	quobyteTenantId := flag.String("tenant_id", "no default", "Id of the Quobyte tenant in whose domain the operation takes place")
-
-	group := flag.String("group", "root", "Group to create the unix socket")
-	maxWaitTime := flag.Float64("max-wait-time", 30, "Maximimum wait time for filesystem checks to complete when a Volume is created before returning an error")
-	maxFSChecks := flag.Int("max-fs-checks", 5, "Maximimum number of filesystem checks when a Volume is created before returning an error")
 	showVersion := flag.Bool("version", false, "Shows version string")
+
 	flag.Parse()
 
+	maxFSChecks, _ := strconv.Atoi(os.Getenv("MAX_FS_CHECKS"))
+	maxWaitTime, _ := strconv.ParseFloat(os.Getenv("MAX_WAIT_TIME"), 64)
+	socketGroup := os.Getenv("SOCKET_GROUP")
+
+	quobyteAPIURL := os.Getenv("QUOBYTE_API_URL")
+	quobyteAPIPassword := os.Getenv("QUOBYTE_API_PASSWORD")
+	quobyteAPIUser := os.Getenv("QUOBYTE_API_USER")
+	quobyteMountPath := os.Getenv("QUOBYTE_MOUNT_PATH")
+	quobyteMountOptions := os.Getenv("QUOBYTE_MOUNT_OPTIONS")
+	quobyteRegistry := os.Getenv("QUOBYTE_REGISTRY")
+	quobyteTenantID := os.Getenv("QUOBYTE_TENANT_ID")
+	quobyteVolConfigName := os.Getenv("QUOBYTE_VOLUME_CONFIG_NAME")
+	log.Printf("\nVariables read from environment:\n"+
+		"MAX_FS_CHECKS: %v\nMAX_WAIT_TIME: %v\nSOCKET_GROUP: %s\n"+
+		"QUOBYTE_API_URL: %s\nQUOBYTE_API_USER: %s\nQUOBYTE_MOUNT_PATH:"+
+		" %s\nQUOBYTE_MOUNT_OPTIONS: %s\nQUOBYTE_REGISTRY: %s\nQUOBYTE_TENANT_ID: "+
+		" %s\nQUOBYTE_VOLUME_CONFIG_NAME: %s\n", maxFSChecks, maxWaitTime,
+		socketGroup, quobyteAPIURL, quobyteAPIUser,
+		quobyteMountPath, quobyteMountOptions, quobyteRegistry, quobyteTenantID,
+		quobyteVolConfigName)
+
 	if *showVersion {
-		log.Printf("Version: %s - Revision: %s\n", version, revision)
+		log.Printf("\nVersion: %s - Revision: %s\n", version, revision)
 		return
 	}
 
-	if err := validateAPIURL(*quobyteAPIURL); err != nil {
+	if err := validateAPIURL(quobyteAPIURL); err != nil {
 		log.Fatalln(err)
 	}
 
-	if err := os.MkdirAll(*quobyteMountPath, 0555); err != nil {
+	if err := os.MkdirAll(quobyteMountPath, 0555); err != nil {
 		log.Println(err.Error())
 	}
 
-	if !isMounted(*quobyteMountPath) {
-		log.Printf("Mounting Quobyte namespace in %s", *quobyteMountPath)
-		mountAll(*quobyteMountOptions, *quobyteRegistry, *quobyteMountPath)
+	if !isMounted(quobyteMountPath) {
+		log.Printf("Mounting Quobyte namespace in %s", quobyteMountPath)
+		mountAll(quobyteMountOptions, quobyteRegistry, quobyteMountPath)
 	}
 
-	qDriver := newQuobyteDriver(*quobyteAPIURL, *quobyteUser, *quobytePassword, *quobyteMountPath, *maxFSChecks, *maxWaitTime, *quobyteConfigName, *quobyteTenantId)
+	qDriver := newQuobyteDriver(quobyteAPIURL, quobyteAPIUser, quobyteAPIPassword,
+		quobyteMountPath, maxFSChecks, maxWaitTime, quobyteVolConfigName, quobyteTenantID)
 	handler := volume.NewHandler(qDriver)
 
-	log.Println(handler.ServeUnix(*group, quobyteID))
+	log.Println(handler.ServeUnix(socketGroup, quobyteID))
 }
